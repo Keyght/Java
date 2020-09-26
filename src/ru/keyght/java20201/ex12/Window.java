b51510cce3b385aea9ded6a70176bbbb6fef95d0
Keyght <nikita_novakov@mail.ru>
2020-09-26 21:36:51 +0300

    ex 12 fixed

--- src/ru/keyght/java20201/ex12/Window.java
+++ src/ru/keyght/java20201/ex12/Window.java
@@ -7,23 +7,22 @@ import java.awt.event.ActionListener;
 
 public class Window extends JFrame {
     public Window() {
-        super("Planet");
         createGUI();
-        JFrame.setDefaultLookAndFeelDecorated(true);
         pack();
-        setLocationRelativeTo(null);
-        setVisible(true);
     }
 
     public void createGUI() {
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame frame = new JFrame("Map");
 
         JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
+        panel.setLayout(null);
 
-        JTextField textField1 = new JTextField("Выберите планету", 15);
-        panel.add(textField1);
+        //JTextField textField1 = new JTextField("Выберите планету", 15);
+        //panel.add(textField1);
+        JLabel label1 = new JLabel("Choose the planet");
+        label1.setBounds(0,0,150,30);
+        label1.setFont(new Font("Times New Roman", Font.BOLD, 14));
+        panel.add(label1);
 
         JComboBox comboBox = new JComboBox();
         comboBox.setEditable(true);
@@ -35,43 +34,49 @@ public class Window extends JFrame {
         comboBox.addItem("Saturn");
         comboBox.addItem("Uranus");
         comboBox.addItem("Neptune");
+        comboBox.setBounds(0,40,120,30);
+        comboBox.setFont(new Font("Times New Roman", Font.BOLD, 14));
         panel.add(comboBox);
 
-        JTextArea textArea = new JTextArea(1, 15);
-        textArea.setText("3,72 m/s^2");
-        panel.add(textArea);
+        JLabel label2 = new JLabel("3,72 m/s^2");
+        label2.setBounds(0,80,150,30);
+        label2.setFont(new Font("Times New Roman", Font.BOLD, 14));
+        panel.add(label2);
 
         comboBox.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (comboBox.getSelectedIndex()==0) {
-                    textArea.setText("3,72 m/s^2");
+                    label2.setText("3,72 m/s^2");
                 }
                 else if (comboBox.getSelectedIndex()==1) {
-                    textArea.setText("8,87 m/s^2");
+                    label2.setText("8,87 m/s^2");
                 }
                 else if (comboBox.getSelectedIndex()==2) {
-                    textArea.setText("9,77 m/s^2");
+                    label2.setText("9,77 m/s^2");
                 }
                 else if (comboBox.getSelectedIndex()==3) {
-                    textArea.setText("3,69 m/s^2");
+                    label2.setText("3,69 m/s^2");
                 }
                 else if (comboBox.getSelectedIndex()==4) {
-                    textArea.setText("20,87 m/s^2");
+                    label2.setText("20,87 m/s^2");
                 }
                 else if (comboBox.getSelectedIndex()==5) {
-                    textArea.setText("7,21 m/s^2");
+                    label2.setText("7,21 m/s^2");
                 }
                 else if (comboBox.getSelectedIndex()==6) {
-                    textArea.setText("8,43 m/s^2");
+                    label2.setText("8,43 m/s^2");
                 }
                 else if (comboBox.getSelectedIndex()==7) {
-                    textArea.setText("10,71 m/s^2");
+                    label2.setText("10,71 m/s^2");
                 }
             }
         });
 
-        getContentPane().add(panel);
-        setPreferredSize(new Dimension(300, 150));
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(160, 160);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
     public class TestActionListener implements ActionListener {
