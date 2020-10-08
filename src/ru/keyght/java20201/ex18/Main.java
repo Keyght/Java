4225d49d5741572e880406bed20bcac3f6c0f6ff
Keyght <nikita_novakov@mail.ru>
2020-10-08 22:33:28 +0300

    add ex 17, 18, 19, 20

--- src/ru/keyght/java20201/ex18/Main.java
+++ src/ru/keyght/java20201/ex18/Main.java
@@ -3,15 +3,25 @@ package ru.keyght.java20201.ex18;
 import java.util.Scanner;
 
 public class Main {
-
     public void exceptionDemo() {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter an integer ");
-        String intString = in.next();
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter an integer");
+        String intString = myScanner.next();
         int i = Integer.parseInt(intString);
-        System.out.println(2 / i);
-    }
+        try {
+            if (2.0/i==Float.POSITIVE_INFINITY) {
+                System.out.println("Получена бесконечность");
+            }
+            else {
+                System.out.println(2.0/i);
+            }
+        } catch (Exception e) {
+            e.getMessage();
+            System.out.println("На ноль делить нельзя");
+        }
 
+
+    }
     public static void main(String[] args) {
         Main main = new Main();
         main.exceptionDemo();
---
+++ src/ru/keyght/java20201/ex20/Files.java
@@ -0,0 +1,24 @@
+package ru.keyght.java20201.ex20;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Files {
+
+    public static void readFiles(File baseDirectory){
+        ArrayList<File> list = new ArrayList<File>();
+        if (baseDirectory.isDirectory()){
+            for (File file : baseDirectory.listFiles()) {
+                list.add(file);
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(list.get(i).getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        File dir = new File("C:\\Users\\Saint\\Desktop");
+        readFiles(dir);
+    }
+}
